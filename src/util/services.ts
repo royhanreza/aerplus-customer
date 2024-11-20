81598fa5495085dc7e6711e5b30960f56f059d17
royhanreza <royhanreza842@gmail.com>
2024-11-20 11:36:17 +0700

    fix: base url api

--- src/util/services.ts
+++ src/util/services.ts
@@ -1,4 +1,4 @@
 // export const baseUrl: string =
 //   "https://de08-2a09-bac1-34a0-28-00-279-43.ngrok-free.app";
-export const baseUrl: string = "aerplus.arenzha.xyz";
+export const baseUrl: string = "https://aerplus.arenzha.xyz";
 export const baseApiUrl: string = `${baseUrl}/api`;
